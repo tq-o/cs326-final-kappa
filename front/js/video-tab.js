f241c49467bd35be160ec19f020a8b475b753dec
Duy Pham <cunbidun@gmail.com>
2020-11-10 01:53:53 +0700

    Add random videos feature

---
+++ front/js/video-tab.js
@@ -0,0 +1,16 @@
+const video_array = [
+  "https://www.youtube.com/embed/GeBog64Ezys",
+  "https://www.youtube.com/embed/RLWcYADoV84",
+  "https://www.youtube.com/embed/FA8tl0fsYdI",
+  "https://www.youtube.com/embed/rA56B4JyTgI",
+  "https://www.youtube.com/embed/FDEYHxtimKk",
+];
+
+const videoTab = document.getElementById("video-tab");
+
+function getRandomInt(num) {
+  let res = Math.floor(Math.random() * 10000000);
+  return res % num;
+}
+
+videoTab.src = video_array[getRandomInt(video_array.length)];
