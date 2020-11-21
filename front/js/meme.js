ec6293affdd714b813aef07bf788bdc91c816b73
tq-o <onquocthai1106@gmail.com>
2020-11-21 17:10:24 -0500

    meme

--- front/js/meme.js
+++ front/js/meme.js
@@ -1,5 +1,8 @@
 const proxyurl = "https://cors-anywhere.herokuapp.com/";
 const url = "http://apimeme.com/meme?meme=Simsimi&top=Top+text&bottom=Bottom+text"; // site that doesn’t send Access-Control-*
 fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
-.then(console.log("Yes"))
+.then((response) => {
+    console.log("Yes");
+    document.getElementById("meme").src = response;
+})
 .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
