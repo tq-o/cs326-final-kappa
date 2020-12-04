35f9d12d5c03ffc0952a4b238c99042414f0c74c
Thanh Phan <phanchithanh091@gmail.com>
2020-12-04 10:01:29 +0700

    refractor sidebar

--- client/js/sidebar.js
+++ client/js/sidebar.js
@@ -1,4 +1,39 @@
 $(document).ready(function () {
+  $("#sidebar").append(`
+    <div id="dismiss">
+      <i class="fas fa-arrow-left"></i>
+    </div>
+
+    <div class="sidebar-header">
+        <h3>Menu</h3>
+    </div>
+
+    <ul class="list-unstyled components">
+      <p>Welcome User's name</p>
+      <li>
+        <a href="activities">Activities Description</a>
+      </li>
+      <li>
+        <a href="welcome">Welcome</a>
+      </li>
+      <li>
+        <a href="breathe">Breathe</a>
+      </li>
+      <li>
+        <a href="meme">Memes</a>
+      </li>
+      <li>
+        <a href="musics">Musics</a>
+      </li>
+      <li>
+        <a href="moodchart">Mood Chart</a>
+      </li>
+      <li>
+        <a href="videos">Videos</a>
+      </li>
+    </ul>
+  `);
+
   $("#sidebar").mCustomScrollbar({
     theme: "minimal",
   });
